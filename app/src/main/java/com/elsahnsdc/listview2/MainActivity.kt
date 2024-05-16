668b1e8f80568df64e6b567512830b4feb79924f
Omar Elsahn <omarelsahn.jmh@gmail.com>
2024-05-16 11:33:20 +0300

    Base Commit

---
+++ app/src/main/java/com/elsahnsdc/listview2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.elsahnsdc.listview2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+//    ext {
+//        //ktx_version = '1.9.0'
+////        splashscreen_version = '1.0.0'
+////
+////        appcompat_version = '1.6.1'
+////        material_version = '1.8.0'
+////        constraintlayout_version = '2.1.4'
+////        junit_version = '4.13.2'
+////        junit_test_version = ':1.1.5'
+////        esspresso_version = '3.5.1'
+//    }
+}
+
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+}
